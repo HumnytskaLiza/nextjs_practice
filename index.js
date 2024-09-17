23c37c399a608da6c399ed5bf210eb2ec235f656
Liza H. <116071539+HumnytskaLiza@users.noreply.github.com>
2024-09-17 08:23:04 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const featureContent = {
+    "simpleBookmarking": {
+        "feature-header": "Bookmark in one click",
+        "feature-desc": "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+        "feature-img": "./assets/illustration-features-tab-1.svg",
+        "feature-img-alt": "feature-tab1-img"
+    },
+    "speedySearching": {
+        "feature-header": "Intelligent search",
+        "feature-desc": "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+        "feature-img": "./assets/illustration-features-tab-2.svg",
+        "feature-img-alt": "feature-tab2-img"
+    },
+    "easySharing": {
+        "feature-header": "Share your bookmarks",
+        "feature-desc": "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+        "feature-img": "./assets/illustration-features-tab-3.svg",
+        "feature-img-alt": "feature-tab3-img"
+    }
+}
+
+let selectedTab = "simpleBookmarking"
+
+function changeTabContent() {
+    
+}
+
